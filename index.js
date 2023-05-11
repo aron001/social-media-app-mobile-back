da710d7d6d1d5f81719e07dc850ddb4bcb045189
0914390824 <aronhunde20@gmail.com>
2023-05-11 22:51:34 +0300

    gitignore

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express =require("express");
-const app= express();
-const mongoose=require("mongoose");
-const dotenv=require("dotenv");
-const helmet=require ("helmet");
-const morgan=require ("morgan");
-const userRoute= require("./routes/users");
-const authRoute= require("./routes/auth");
-const postRoute= require("./routes/posts");
-const quationRoute= require("./routes/quations");
-const answerRoute= require("./routes/answer");
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const quationRoute = require("./routes/quations");
+const answerRoute = require("./routes/answer");
 dotenv.config();
 //mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifieldTopology: true},()=>{
 //console.log("connected to mongodb")
 //});
-mongoose.connect(process.env.MONGO_URL,()=>console.log('database connected'));
+mongoose.connect(process.env.MONGO_URL, () =>
+    console.log("database connected")
+);
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
@@ -21,8 +23,8 @@ app.use(morgan("common"));
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
-app.use("/api/quations", quationRoute)
-app.use("/api/answers", answerRoute)
-app.listen(3000,()=>{
-    console.log("backend server is running on port 5000")
-})
+app.use("/api/quations", quationRoute);
+app.use("/api/answers", answerRoute);
+app.listen(3000, () => {
+    console.log("backend server is running on port 5000");
+});
